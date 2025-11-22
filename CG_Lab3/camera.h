0b056125850d39483dffc4b1ef06f5cbd806b762
Dentera <artem.loskutov@gmail.com>
2025-11-22 11:50:17 +0300

    Added some interesting code

---
+++ CG_Lab3/camera.h
@@ -0,0 +1,21 @@
+#ifndef __CAMERA_H__
+#define __CAMERA_H__
+
+#include "geometry.h"
+
+
+class Camera 
+{
+private:
+	Vec3f eye_;
+	Vec3f center_;
+	Vec3f up_;
+public:
+	Camera(Vec3f eye, Vec3f center, Vec3f up) : eye_(eye), center_(center), up_(up){}
+	Matrix viewportMatrix(int x, int y, int w, int h, int d);
+	Matrix lookat(Vec3f eye, Vec3f center, Vec3f up);
+	Matrix proj(Vec3f eye, Vec3f center);
+};
+
+
+#endif //__CAMERA_H__
